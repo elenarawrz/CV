d9488606a80990d714d09adb361e249f6fb48a7e
elenavalle <elenavaller@hotmail.com>
2017-04-03 13:42:32 -0500

    further refactoring

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 $(() => {
   // contactInfo, dataEng & dataEsp from './info.js'
-  var data = dataEng, content,
+  var data = dataEng,
 
   setTitle = section => {
     $(`#${section} > h3`).text(data[section].title);
@@ -8,10 +8,7 @@ $(() => {
 
   load = (section, callback) => {
     setTitle(section);
-
-    content = '';
-    data[section].content.forEach(callback);
-
+    content = data[section].content.reduce(callback, '');
     $(`#${section}`).append(content);
   },
 
@@ -21,8 +18,8 @@ $(() => {
           Object.keys(contactInfo)
                 .forEach(key => $(`#${key}`).text(contactInfo[key]));
         },
-    () => load('summary', p =>  content += `<p>${p}</p>`),
-    () => load('workExperience', work => content += `<p>
+    () => load('summary', (acc, p) =>  acc += `<p>${p}</p>`),
+    () => load('workExperience', (acc, work) => acc += `<p>
           <span class='primary'>${work.position} |</span>
           ${work.company} |
           <span class='secondary'>${work.period}</span>
@@ -37,12 +34,12 @@ $(() => {
           $(`#period`).text(data.education.period);
           $(`#major`).text(data.education.major);
         },
-    () => load('languages', lang => content += `<p>
+    () => load('languages', (acc, lang) => acc += `<p>
           <span class='primary'>${lang.language} |</span>
           ${lang.level}
         </p>`),
-    () => load('technologies', tech => content += `<p>${tech}</p>`),
-    () => load('certifications', cert => content += `<p>
+    () => load('technologies', (acc, tech) => acc += `<p>${tech}</p>`),
+    () => load('certifications', (acc, cert) => acc += `<p>
         ${cert.name} |
         <span class='secondary'>${cert.date}</span>
       </p>`)
