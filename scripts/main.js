141219edf36523a58b681e827855e0dd264dc19c
elenavalle <elenavaller@hotmail.com>
2017-04-03 13:26:11 -0500

    refactor info loaders

--- scripts/main.js
+++ scripts/main.js
@@ -1,80 +1,52 @@
 $(() => {
   // contactInfo, dataEng & dataEsp from './info.js'
-  var data = dataEng,
-
-  loadContactDetails = () => {
-    setTitle('contactDetails')
-    Object.keys(contactInfo)
-          .forEach(key => $(`#${key}`).text(contactInfo[key]));
-  },
-
-  loadSummary = obj => {
-    setTitle('summary');
-    var content = '';
-    obj.content.forEach(p =>  content += `<p>${p}</p>`);
-    $('#summary').append(content);
-  },
-
-  loadEducation = obj => {
-    setTitle('education');
-    $(`#school`).text(obj.school + ' |');
-    $(`#period`).text(obj.period);
-    $(`#major`).text(obj.major);
-  },
-
-  loadWorkExperience = obj => {
-    setTitle('workExperience');
-    var content = '';
-    obj.content.forEach(work => content += `<p>
-        <span class='primary'>${work.position} |</span>
-        ${work.company} |
-        <span class='secondary'>${work.period}</span>
-      </p>
-      <p>
-        <span id='activities'>${work.activities}</span>
-        (<span id='tools'>${work.tools}</span>)
-      </p>`
-    );
-    $('#workExperience').append(content);
-  },
-
-  loadLanguages = obj => {
-    setTitle('languages');
-    var content = '';
-    obj.content.forEach(lang => content += `<p>
-        <span class='primary'>${lang.language} |</span>
-        ${lang.level}
-      </p>`
-    );
-    $('#languages').append(content);
-  },
-
-  loadTechnologies = obj => {
-    setTitle('technologies');
-    var content = '';
-    obj.content.forEach(tech => content += `<p>${tech}</p>`);
-    $('#technologies').append(content);
-  },
-
-  loadCertifications = obj => {
-    setTitle('certifications');
-    var content = '';
-    obj.content.forEach(cert => content += `<p>
-        ${cert.name} |
-        <span class='secondary'>${cert.date}</span>
-      </p>`);
-    $('#certifications').append(content);
-  },
+  var data = dataEng, content,
 
   setTitle = section => {
     $(`#${section} > h3`).text(data[section].title);
-  };
+  },
 
-  loadContactDetails();
-  loadSummary(data.summary);
-  loadEducation(data.education);
-  loadWorkExperience(data.workExperience);
-  loadLanguages(data.languages);
-  loadTechnologies(data.technologies);
-  loadCertifications(data.certifications);
+  load = (section, callback) => {
+    setTitle(section);
+
+    content = '';
+    data[section].content.forEach(callback);
+
+    $(`#${section}`).append(content);
+  },
+
+  loaders = [
+    () => {
+          setTitle('contactDetails');
+          Object.keys(contactInfo)
+                .forEach(key => $(`#${key}`).text(contactInfo[key]));
+        },
+    () => load('summary', p =>  content += `<p>${p}</p>`),
+    () => load('workExperience', work => content += `<p>
+          <span class='primary'>${work.position} |</span>
+          ${work.company} |
+          <span class='secondary'>${work.period}</span>
+        </p>
+        <p>
+          <span id='activities'>${work.activities}</span>
+          (<span id='tools'>${work.tools}</span>)
+        </p>`),
+    () => {
+          setTitle('education');
+          $(`#school`).text(data.education.school + ' |');
+          $(`#period`).text(data.education.period);
+          $(`#major`).text(data.education.major);
+        },
+    () => load('languages', lang => content += `<p>
+          <span class='primary'>${lang.language} |</span>
+          ${lang.level}
+        </p>`),
+    () => load('technologies', tech => content += `<p>${tech}</p>`),
+    () => load('certifications', cert => content += `<p>
+        ${cert.name} |
+        <span class='secondary'>${cert.date}</span>
+      </p>`)
+  ];
+
+  loaders.forEach(loader => loader());
 });
